a3213f28eb51dcdc1673155e9e9cc0af77135102
florienn <florienn@gmail.com>
2019-12-02 17:08:15 +0100

    portfolio update

--- orders/views.py
+++ orders/views.py
@@ -77,7 +77,7 @@ def login_view(request):
          if user is not None:
              login(request, user)
              return HttpResponseRedirect('/')
-         return render(request, 'orders/error.html', {'message_danger': 'Invalid username/password.'})
+         return render(request, 'orders/login.html', {'message_danger': 'Invalid username/password.'})
     return render(request, 'orders/login.html')
 
 
